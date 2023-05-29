1b9052140030ab6516937243971466df360c1979
Dokichan <bironincres@gmail.com>
2023-05-29 20:50:02 +0300

    Dispose и Title

--- ShogiGames/ViewModels/MainWindowViewModel.cs
+++ ShogiGames/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShogiGames.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace ShogiGames.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Сёги 将棋";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);          
+        }
+        #endregion
     }
 }
